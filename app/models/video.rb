9899ae80c9b9b99643d451fe5246f2ccc1db38d3
Maya <maya_sato@diveintocode.jp>
2018-10-30 16:26:05 +0900

    URLしか投稿できないようにする

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,7 @@
 class Video < ApplicationRecord
   validates :title,    length: { in: 1..20 }
   validates :url,      length: { in: 1..400 }
+  validates :url, format: /\A#{URI::regexp(%w(http https))}\z/
   validates :status,   length: { in: 1..11 }
   belongs_to :user
   has_many :comments, dependent: :destroy
