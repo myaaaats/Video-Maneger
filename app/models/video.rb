d2a5da152e84c6542fd1711caa793da4225f0a21
Maya <maya_sato@diveintocode.jp>
2018-10-30 16:30:14 +0900

    同じURLを投稿できないようにする

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,8 +1,8 @@
 class Video < ApplicationRecord
   validates :title,    length: { in: 1..20 }
-  validates :url,      length: { in: 1..400 }
-  validates :url, format: /\A#{URI::regexp(%w(http https))}\z/
+  validates :url,      length: { in: 1..400 }, uniqueness: true
+  validates :url,      format: /\A#{URI::regexp(%w(http https))}\z/
   validates :status,   length: { in: 1..11 }
   belongs_to :user
-  has_many :comments, dependent: :destroy
+  has_many :comments,  dependent: :destroy
 end
