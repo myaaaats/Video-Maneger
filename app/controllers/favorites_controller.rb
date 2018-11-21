8d6f385d3c37fb975d5a2066be92979c68f40b13
Maya <maya_sato@diveintocode.jp>
2018-11-21 18:56:42 +0900

    お気に入りボタンを押した時のリダイレクトページを変更

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,11 +5,11 @@ class FavoritesController < ApplicationController
 
   def create
     favorite = current_user.favorites.create(video_id: params[:video_id])
-    redirect_to videos_url, notice: "お気に入り登録しました"
+    redirect_to favorites_path, notice: "お気に入り登録しました"
   end
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to videos_url, notice: "お気に入り解除しました"
+    redirect_to favorites_path, notice: "お気に入り解除しました"
   end
 end
