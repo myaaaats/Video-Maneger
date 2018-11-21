a119ebf3f8bdfb498ea5f485a6f60a049f6dd186
Maya <maya_sato@diveintocode.jp>
2018-11-21 20:09:34 +0900

    ステータス絞り込みを実装(view反映以外)---エラー箇所の修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :videos do
     resources :comments
-    get 'filter', on: :member
+    get 'index', on: :member
     collection do
       post :confirm
     end
