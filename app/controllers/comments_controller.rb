7b863f8b58d22364fb738f16a5dd4c222b138e8b
Maya <maya_sato@diveintocode.jp>
2018-11-22 12:03:41 +0900

    コメントの削除にAjaxを用いて、画面遷移しないようにする

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,18 +6,17 @@ class CommentsController < ApplicationController
       if @comment.save
         format.js { render :index }
       else
-        #error.js.erb（textボックスの枠を赤くするなど）ものを動かす
         format.js { render :error }
-        #format.html { redirect_to video_path(@video), notice: '投稿できませんでした...' }
       end
     end
   end
 
   def destroy
     @comment = Comment.find(params[:id])
-    @comment.destroy
     #comment投稿したときみたいにindex.js.erbを呼ぶ
-    redirect_to video_path(@comment.video_id), notice:"コメントを削除しました！"
+      if @comment.destroy
+        render 'index'
+      end
   end
 
   private
