0844722e287f39641a04d0e0ccd205ab9f63d44e
Maya <maya_sato@diveintocode.jp>
2018-11-22 13:38:24 +0900

    URLとUser_idの組み合わせがユニークかどうかバリデーションをかける

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,6 +2,7 @@ class Video < ApplicationRecord
   validates :title,    length: { in: 1..20 }
   validates :url,      length: { in: 1..400 }
   validates :url,      format: /\A#{URI::regexp(%w(http https))}\z/
+  validates :url,      :uniqueness => {:scope => :user_id},  acceptance: { message: 'は既に投稿されたものです' }
   validates :status,   length: { in: 1..11 }
   belongs_to :user
   has_many :comments,  dependent: :destroy
