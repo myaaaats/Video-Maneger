9ca995ac08cbc6b64f98e5c4e2898a0bb2293a7a
Maya <maya_sato@diveintocode.jp>
2018-11-24 16:22:17 +0900

    動画投稿フォームのエラーメッセージを明確にする

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,8 +3,8 @@ class Video < ApplicationRecord
   validates :url,      length: { in: 1..400 }
   validates :url,      format: /\A#{URI::regexp(%w(http https))}\z/
   validates :url,      :uniqueness => {:scope => [:user_id], message: 'は既に投稿されたものです'}
-  validates :url,      format: { with: /google/ }
-  validates :url,      format: { with: /drive/ }
+  validates :url,      format: { with: /google/, message: 'は「https://drive.google.com」を含んだアドレスにしてください'}
+  validates :url,      format: { with: /drive/, message: 'は「https://drive.google.com」を含んだアドレスにしてください' }
   validates :status,   length: { in: 1..11 }
   belongs_to :user
   has_many :comments,  dependent: :destroy
